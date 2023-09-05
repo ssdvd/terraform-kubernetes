7861b0b48d7dace88abaeb81baac4aa021027052
ssdvd <david.santana98@hotmail.com>
2023-09-04 23:35:07 -0300

    config kubernetes probe

---
+++ infra/kubernetes.tf
@@ -0,0 +1,59 @@
+resource "kubernetes_deployment" "django-api" {
+  metadata {
+    name = "django-api"
+    labels = {
+      nome = "django"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        nome = "django"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          nome = "django"
+        }
+      }
+
+      spec {
+        container {
+          image = "027662851187.dkr.ecr.us-east-2.amazonaws.com/producao:v1"
+          name  = "django"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 80
+
+              http_header {
+                name  = "X-Custom-Header"
+                value = "Awesome"
+              }
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
+  }
+}
